4848271b708b6963d91570fe90a7b2020a9bd431
Jesse Chan <jesse.chan@rice.edu>
2020-09-16 11:08:36 -0500

    fixed iterator

--- src/multi_indices.jl
+++ src/multi_indices.jl
@@ -1,5 +1,4 @@
-# routines to increment a multi-index of arbitrary dimension and degree
-
+"iterator type over simplicial multi-indices"
 struct MultiIndexSet
     d::Int # dimension
     N::Int # degree
@@ -7,25 +6,28 @@ end
 
 Base.length(a::MultiIndexSet) = prod(ntuple(i->(a.N+i),a.d)) ÷ factorial(a.d)
 
-function Base.iterate(set::MultiIndexSet,
-                      α::NTuple=ntuple(x->zero(Int),set.d))
-    return 1,α
+function Base.iterate(set::MultiIndexSet)
+    α = ntuple(x->zero(Int),set.d)
+    return α,α
 end
-
 function Base.iterate(set::MultiIndexSet,α::NTuple)
-    if last(α) > set.N
+    if last(α) == set.N
         return nothing
     else
-        return α,increment(α,set,1)
+        αnext = increment(α,set,1)
+        return αnext,αnext
     end
 end
 
+"function increment(I::NTuple,set::MultiIndexSet,i::Int):
+    increments a multi-index of arbitrary dimension and degree.
+    Intended to be used within the interface for Base.iterate"
 function increment(I::NTuple,set::MultiIndexSet,i::Int)
     if length(I) != set.d
         error(DimensionMismatch("Length of I = $(length(I)) while set.d = $(set.d)"))
     end
     if last(I)==set.N
-        return (I[1:end-1]...,last(I)+1) # "incorrect" state to end iteration
+        return (I[1:end-1]...,last(I)+1) # set "incorrect" state to end iteration
     end
     if sum(I) < set.N
         return (I[1:i-1]...,I[i]+1,I[i+1:end]...)
